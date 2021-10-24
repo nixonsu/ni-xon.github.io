566a7948ac13afb5be5a9544b15b610aed623e43
ni-xon <nixonuch_@hotmail.com>
2021-10-25 00:23:16 +1100

    Added font sizes

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const target = document.body.querySelector(".typewrite");
 const speed = target.getAttribute("data-speed");
 
 // Constructing Queue
-let listofPhrases = ["Hello there", "I'm Nixon"];
+let listofPhrases = ["Hello there,", "I'm Nixon"];
 let phrasesQueue = new Queue();
 listofPhrases.forEach((phrase) => {
   phrasesQueue.enqueue(phrase);
