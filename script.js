1b00d6ec944dcd26858fdd7c44b6bdf8fe9c4999
ni-xon <nixonuch_@hotmail.com>
2021-10-28 23:19:02 +1100

    Modified landing text

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const speed = target.getAttribute("data-speed");
 let listofPhrases = [
   "Hi, nice to meet you",
   "I'm Nixon,",
-  "I'm a student, software developer and a data analyst",
+  "A student, software developer and a data analyst",
 ];
 let phrasesQueue = new Queue();
 listofPhrases.forEach((phrase) => {
